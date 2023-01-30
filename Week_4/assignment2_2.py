45710d8061b18e9d3b717f44f47ca79560ab23cf
Javier Nicolás González <89177014+nicolasgonzalez78837@users.noreply.github.com>
2023-01-30 19:45:45 -0300

    Update assignment2_2.py

--- Week_4/assignment2_2.py
+++ Week_4/assignment2_2.py
@@ -1,2 +1,8 @@
+'''
+Write a program that uses input to prompt a user for their name and then welcomes them. 
+Note that input will pop up a dialog box. 
+Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+'''
+
 name = input('Enter Your name: ')
 print("Hello",name)
